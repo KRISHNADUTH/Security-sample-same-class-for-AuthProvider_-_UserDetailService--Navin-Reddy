d90e743e81a83170c889e84beaba47a551faf58b
Krishnaduth <krishnaduth777@gmail.com>
2024-10-11 07:18:01 +0530

    Added exception handling in JwtFilter

--- src/main/java/com/example/demo/filter/JwtFilter.java
+++ src/main/java/com/example/demo/filter/JwtFilter.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,9 +13,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.demo.config.MyUserDetailService;
+import com.example.demo.dto.ErrorResponseDto;
 import com.example.demo.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.FilterChain;
+import java.util.Date;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,35 +30,40 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
     private ApplicationContext context;
-    
+
     @Autowired
     private JwtService jwtTokenService;
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        System.out.println("JwtTokenValidationFilter IS CAAAAAAALEEDDDDDDDDDDDDDDDD");
         String authorizationHeader = request.getHeader("Authorization");
-        System.out.println("Authorization from request isssssssssssssssss = "+authorizationHeader);
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            System.out.println("Inside IF Authorization from request isssssssssssssssss = "+authorizationHeader);
             String jwtToken = authorizationHeader.substring(7);
-            String username = jwtTokenService.extractUsername(jwtToken);
-            System.out.println("Extracted user name issssssss = "+username);
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = context.getBean(MyUserDetailService.class).loadUserByUsername(username);
-                if (jwtTokenService.validateToken(jwtToken, userDetails)) {
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                                                                                userDetails.getUsername(), null, userDetails.getAuthorities());
-                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                } else {
-                    throw new BadCredentialsException("JWT token validation failed........");
+            try {
+                String username = jwtTokenService.extractUsername(jwtToken);
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = context.getBean(MyUserDetailService.class).loadUserByUsername(username);
+                    if (jwtTokenService.validateToken(jwtToken, userDetails)) {
+                        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                userDetails.getUsername(), null, userDetails.getAuthorities());
+                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
                 }
+            } catch (Exception e) {
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.setStatus(401);
+                ErrorResponseDto errorResponseDto = new ErrorResponseDto(request.getServletPath(),
+                        HttpStatus.UNAUTHORIZED, e.getMessage(), new Date());
+                ObjectMapper objectMapper = new ObjectMapper();
+                response.getWriter().write(objectMapper.writeValueAsString(errorResponseDto));
+                response.getWriter().flush();
+                return;
             }
 
-        }
+        }P
         filterChain.doFilter(request, response);
     }
 
